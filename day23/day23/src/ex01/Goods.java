9bb1aaf0699ff1779757084cfd2bdf4f00b2f3c8
유요한 <110465572+YuYoHan@users.noreply.github.com>
2024-06-19 14:02:56 +0900

    [Refactor] 리팩토링

--- day23/day23/src/ex01/Goods.java
+++ day23/day23/src/ex01/Goods.java
@@ -1,204 +1,204 @@
-package ex01;
-
-
-import ex01.dao.GoodsDAO;
-import ex01.dto.GoodsDTO;
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.util.ArrayList;
-import java.util.Vector;
-
-public class Goods extends JFrame {
-    JTextField jtf01;
-    JTextField jtf02;
-    JTextField jtf03;
-    JTextField jtf04;
-    Index mainFrame;
-    GoodsDTO goodsDTO;
-    Vector<String> colNames;
-    Vector<Vector<String>> rowData;
-    JTable table;
-    int numId;
-    String item;
-    int qty;
-    int price;
-
-    public Goods() {
-//        this.mainFrame = mainFrame;
-
-        colNames = new Vector<>();
-        colNames.add("상품번호");
-        colNames.add("상품이름");
-        colNames.add("상품수량");
-        colNames.add("상품가격");
-
-        rowData = new Vector<>();
-        table = new JTable(rowData, colNames);
-        JScrollPane jsp = new JScrollPane(table);
-
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(4, 2));
-        panel.add(new JLabel("상품 번호 :"));
-        jtf01 = new JTextField();
-        panel.add(jtf01);
-        panel.add(new JLabel("상품명 : "));
-        jtf02 = new JTextField();
-        panel.add(jtf02);
-        panel.add(new JLabel("수량 : "));
-        jtf03 = new JTextField();
-        panel.add(jtf03);
-        panel.add(new JLabel("가격 : "));
-        jtf04 = new JTextField();
-        panel.add(jtf04);
-
-        JButton button = new JButton("등록");
-        JButton button_update = new JButton("수정");
-        JButton button_delete = new JButton("삭제");
-        JPanel panel2 = new JPanel();
-        panel2.setLayout(new FlowLayout());
-        panel2.add(button);
-        panel2.add(button_update);
-        panel2.add(button_delete);
-
-        JPanel p_north = new JPanel();
-        p_north.setLayout(new BorderLayout());
-        p_north.add(panel, BorderLayout.CENTER);
-        p_north.add(panel2, BorderLayout.SOUTH);
-
-        // 패널을 프레임에 추가
-        add(p_north, BorderLayout.NORTH);
-        add(jsp, BorderLayout.CENTER);
-        addVector();
-        setSize(400, 300);
-        setVisible(true);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-        table.addMouseListener(new MouseListener() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mousePressed(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                int index = table.getSelectedRow();
-                Vector<String> row = rowData.get(index);
-                System.out.println(row.get(0));
-                jtf01.setText(row.get(0));
-                jtf02.setText(row.get(1));
-                jtf03.setText(row.get(2));
-                jtf04.setText(row.get(3));
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-
-            }
-        });
-
-        // 등록
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                setData();
-                goodsDTO = new GoodsDTO();
-                goodsDTO.setNumId(numId);
-                goodsDTO.setItem(item);
-                goodsDTO.setQty(qty);
-                goodsDTO.setPrice(price);
-                System.out.println(goodsDTO);
-                int insert = GoodsDAO.insert(goodsDTO);
-                System.out.println("등록한 개수 : " + insert);
-                if (insert > 0) {
-                    JOptionPane.showMessageDialog(null, "상품 정보 등록 성공");
-                    addVector();
-                    mainFrame.showBoard();
-                } else {
-                    JOptionPane.showMessageDialog(null, "상품 정보 등록 실패");
-                }
-            }
-        });
-
-        // 수정
-        button_update.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                setData();
-                GoodsDTO goods = new GoodsDTO();
-                goods.setItem(item);
-                goods.setQty(qty);
-                goods.setPrice(price);
-                System.out.println(goods);
-                int update = GoodsDAO.update(goods);
-                if (update > 0) {
-                    JOptionPane.showMessageDialog(null, "상품 정보 수정 성공");
-                    addVector();
-                    mainFrame.showBoard();
-                } else {
-                    JOptionPane.showMessageDialog(null, "상품 정보 수정 실패");
-                }
-            }
-        });
-
-        // 삭제
-        button_delete.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                setData();
-                int delete = GoodsDAO.delete(numId);
-                if (delete > 0) {
-                    JOptionPane.showMessageDialog(null, "상품 삭제 성공");
-                    addVector();
-                    mainFrame.showBoard();
-                } else {
-                    JOptionPane.showMessageDialog(null, "상품 삭제 실패");
-                }
-            }
-        });
-    }
-
-    // 텍스트 필드의 입력값을 읽어와서 각각의 변수에 저장하는 메서드
-    public void setData() {
-        numId = Integer.parseInt(jtf01.getText());
-        item = jtf02.getText();
-        qty = Integer.parseInt(jtf03.getText());
-        price = Integer.parseInt(jtf04.getText());
-    }
-
-    // 모든 상품목록을 읽어와서 JTable에 출력하는 메서드
-    public void addVector() {
-        rowData.clear();
-        ArrayList<GoodsDTO> select = GoodsDAO.select();
-        System.out.println(select);
-        Vector<String> v = new Vector<>();
-        for (GoodsDTO goodsDTO : select) {
-            v.add(goodsDTO.getNumId() + "");
-            v.add(goodsDTO.getItem());
-            v.add(goodsDTO.getQty() + "");
-            v.add(goodsDTO.getPrice() + "");
-            rowData.add(v);
-
-        }
-        System.out.println("등록 성공");
-        table.updateUI();
-    }
-
-    public static void main(String[] args) {
-        new Goods();
-    }
-}
+//package ex01;
+//
+//
+//import ex01.dao.GoodsDAO;
+//import ex01.dto.GoodsDTO;
+//
+//import javax.swing.*;
+//import java.awt.*;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
+//import java.awt.event.MouseEvent;
+//import java.awt.event.MouseListener;
+//import java.util.ArrayList;
+//import java.util.Vector;
+//
+//public class Goods extends JFrame {
+//    JTextField jtf01;
+//    JTextField jtf02;
+//    JTextField jtf03;
+//    JTextField jtf04;
+//    Index mainFrame;
+//    GoodsDTO goodsDTO;
+//    Vector<String> colNames;
+//    Vector<Vector<String>> rowData;
+//    JTable table;
+//    int numId;
+//    String item;
+//    int qty;
+//    int price;
+//
+//    public Goods() {
+////        this.mainFrame = mainFrame;
+//
+//        colNames = new Vector<>();
+//        colNames.add("상품번호");
+//        colNames.add("상품이름");
+//        colNames.add("상품수량");
+//        colNames.add("상품가격");
+//
+//        rowData = new Vector<>();
+//        table = new JTable(rowData, colNames);
+//        JScrollPane jsp = new JScrollPane(table);
+//
+//        JPanel panel = new JPanel();
+//        panel.setLayout(new GridLayout(4, 2));
+//        panel.add(new JLabel("상품 번호 :"));
+//        jtf01 = new JTextField();
+//        panel.add(jtf01);
+//        panel.add(new JLabel("상품명 : "));
+//        jtf02 = new JTextField();
+//        panel.add(jtf02);
+//        panel.add(new JLabel("수량 : "));
+//        jtf03 = new JTextField();
+//        panel.add(jtf03);
+//        panel.add(new JLabel("가격 : "));
+//        jtf04 = new JTextField();
+//        panel.add(jtf04);
+//
+//        JButton button = new JButton("등록");
+//        JButton button_update = new JButton("수정");
+//        JButton button_delete = new JButton("삭제");
+//        JPanel panel2 = new JPanel();
+//        panel2.setLayout(new FlowLayout());
+//        panel2.add(button);
+//        panel2.add(button_update);
+//        panel2.add(button_delete);
+//
+//        JPanel p_north = new JPanel();
+//        p_north.setLayout(new BorderLayout());
+//        p_north.add(panel, BorderLayout.CENTER);
+//        p_north.add(panel2, BorderLayout.SOUTH);
+//
+//        // 패널을 프레임에 추가
+//        add(p_north, BorderLayout.NORTH);
+//        add(jsp, BorderLayout.CENTER);
+//        addVector();
+//        setSize(400, 300);
+//        setVisible(true);
+//        setDefaultCloseOperation(EXIT_ON_CLOSE);
+//
+//        table.addMouseListener(new MouseListener() {
+//            @Override
+//            public void mouseClicked(MouseEvent e) {
+//
+//            }
+//
+//            @Override
+//            public void mousePressed(MouseEvent e) {
+//
+//            }
+//
+//            @Override
+//            public void mouseReleased(MouseEvent e) {
+//                int index = table.getSelectedRow();
+//                Vector<String> row = rowData.get(index);
+//                System.out.println(row.get(0));
+//                jtf01.setText(row.get(0));
+//                jtf02.setText(row.get(1));
+//                jtf03.setText(row.get(2));
+//                jtf04.setText(row.get(3));
+//            }
+//
+//            @Override
+//            public void mouseEntered(MouseEvent e) {
+//
+//            }
+//
+//            @Override
+//            public void mouseExited(MouseEvent e) {
+//
+//            }
+//        });
+//
+//        // 등록
+//        button.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                setData();
+//                goodsDTO = new GoodsDTO();
+//                goodsDTO.setNumId(numId);
+//                goodsDTO.setItem(item);
+//                goodsDTO.setQty(qty);
+//                goodsDTO.setPrice(price);
+//                System.out.println(goodsDTO);
+//                int insert = GoodsDAO.insert(goodsDTO);
+//                System.out.println("등록한 개수 : " + insert);
+//                if (insert > 0) {
+//                    JOptionPane.showMessageDialog(null, "상품 정보 등록 성공");
+//                    addVector();
+//                    mainFrame.showBoard();
+//                } else {
+//                    JOptionPane.showMessageDialog(null, "상품 정보 등록 실패");
+//                }
+//            }
+//        });
+//
+//        // 수정
+//        button_update.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                setData();
+//                GoodsDTO goods = new GoodsDTO();
+//                goods.setItem(item);
+//                goods.setQty(qty);
+//                goods.setPrice(price);
+//                System.out.println(goods);
+//                int update = GoodsDAO.update(goods);
+//                if (update > 0) {
+//                    JOptionPane.showMessageDialog(null, "상품 정보 수정 성공");
+//                    addVector();
+//                    mainFrame.showBoard();
+//                } else {
+//                    JOptionPane.showMessageDialog(null, "상품 정보 수정 실패");
+//                }
+//            }
+//        });
+//
+//        // 삭제
+//        button_delete.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                setData();
+//                int delete = GoodsDAO.delete(numId);
+//                if (delete > 0) {
+//                    JOptionPane.showMessageDialog(null, "상품 삭제 성공");
+//                    addVector();
+//                    mainFrame.showBoard();
+//                } else {
+//                    JOptionPane.showMessageDialog(null, "상품 삭제 실패");
+//                }
+//            }
+//        });
+//    }
+//
+//    // 텍스트 필드의 입력값을 읽어와서 각각의 변수에 저장하는 메서드
+//    public void setData() {
+//        numId = Integer.parseInt(jtf01.getText());
+//        item = jtf02.getText();
+//        qty = Integer.parseInt(jtf03.getText());
+//        price = Integer.parseInt(jtf04.getText());
+//    }
+//
+//    // 모든 상품목록을 읽어와서 JTable에 출력하는 메서드
+//    public void addVector() {
+//        rowData.clear();
+//        ArrayList<GoodsDTO> select = GoodsDAO.select();
+//        System.out.println(select);
+//        Vector<String> v = new Vector<>();
+//        for (GoodsDTO goodsDTO : select) {
+//            v.add(goodsDTO.getNumId() + "");
+//            v.add(goodsDTO.getItem());
+//            v.add(goodsDTO.getQty() + "");
+//            v.add(goodsDTO.getPrice() + "");
+//            rowData.add(v);
+//
+//        }
+//        System.out.println("등록 성공");
+//        table.updateUI();
+//    }
+//
+//    public static void main(String[] args) {
+//        new Goods();
+//    }
+//}
--- day23/day23/src/ex02/Index.java
+++ day23/day23/src/ex02/Index.java
@@ -26,12 +26,10 @@ public class Index extends JFrame {
         jPanel2.add(signUpButton);
         JButton loginButton = new JButton("로그인");
         jPanel2.add(loginButton);
-        JButton exitButton = new JButton("나가기");
-        jPanel2.add(exitButton);
 
         setLayout(new BorderLayout());
         add(jPanel, BorderLayout.CENTER);
-        add(jPanel2, BorderLayout.SOUTH);
+        add(jPanel2, BorderLayout.NORTH);
 
         setSize(400, 300);
         setVisible(true);
@@ -53,13 +51,6 @@ public class Index extends JFrame {
                 setVisible(false); // 현재 창 숨기기
             }
         });
-        // 나가기
-        exitButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                dispose(); // 현재 윈도우를 닫음
-            }
-        });
     }
     // 로그인 후 보이는 창
     public void afterLogin() {
@@ -69,7 +60,20 @@ public class Index extends JFrame {
         System.out.println("현재 접속중인 닉네임 : " + currentUser);
 
         JButton logout_button = new JButton("로그아웃");
+        JButton myInfo_button = new JButton("내정보보기");
+        JButton board_button = new JButton("게시물");
 
+        JPanel jPanel = new JPanel();
+        jPanel.add(logout_button);
+        jPanel.add(myInfo_button);
+        jPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        setLayout(new BorderLayout());
+        add(jPanel, BorderLayout.NORTH);
+
+        JPanel jPanel2 = new JPanel();
+        jPanel2.setLayout(new FlowLayout(FlowLayout.CENTER));
+        jPanel2.add(board_button);
+        add(jPanel2, BorderLayout.CENTER);
 
         logout_button.addActionListener(new ActionListener() {
             @Override
@@ -78,6 +82,17 @@ public class Index extends JFrame {
                 SessionManager.logoutUser();
                 dispose(); // 현재 윈도우를 닫음
                 setVisible(true);
+            }
+        });
+
+        board_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("게시글 메뉴를 선택하였습니다.");
+                boolean check = SessionManager.isLoggedIn();
+                if(check) {
+                    return;
+                }
             }
         });
 
